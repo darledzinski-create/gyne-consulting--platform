a328be0cc4bb0783dbe4e56ba2eec727c4f22d36
darledzinski-create <darledzinski@gmail.com>
2025-11-26 09:27:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/ask")
+def ask_page():
+    return render_template("ask.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
