69b77040bb37d0f10f5ad2663b6da7be357dfd62
darledzinski-create <darledzinski@gmail.com>
2026-01-11 17:11:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,9 @@ def intake():
             "conditions": request.form.get("conditions"),
             "medications": request.form.get("medications"),
             "allergies": request.form.get("allergies"),
+
+        }
+        
         print("ABOUT TO CALL send_doctor_email")
         send_doctor_email(intake_data)
         print("RETURNING RESPONSE")
