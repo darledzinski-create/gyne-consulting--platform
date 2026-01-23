3ef0f41de857dd16f6ef6354119f8b55829ff076
darledzinski-create <darledzinski@gmail.com>
2026-01-23 19:24:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,14 +17,14 @@ def intake():
             "full_name": request.form.get("full_name"),
             "email": request.form.get("email"),
             "phone": request.form.get("phone"),
-            "concern": request.form.get("concern")
+            "concern": request.form.get("concern"),
+        }
     if emergency:
             send_emergency_sms(
                 full_name=intake_data["full_name"],
                 phone=intake_data["phone"],
                 concern=intake_data["concern"]
             )
-        }
 
         print("INTAKE RECEIVED:", intake_data)
         return render_template("thank_you.html")
