4bc7f1f634796e62fe0d77c98df2e929092ac4c9
darledzinski-create <darledzinski@gmail.com>
2026-04-01 19:14:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "LOCAL TEST WORKING"
-    print("REQUEST METHOD:", request.method)
-
     if request.method == "POST":
-        print("POST DETECTED ✅")
-
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
 
-        print("DATA:", name, email, message)
-        return render_template("index.html", success=True)
+        print("FORM RECEIVED:", name, email, message)
 
-    return render_template("index.html", success=False)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template("index.html")
