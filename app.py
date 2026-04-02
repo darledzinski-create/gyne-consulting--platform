93c6312294a3b4a032bc4a796bb9c71a61226486
darledzinski-create <darledzinski@gmail.com>
2026-04-02 18:51:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    print("REQUEST METHOD:", request.method)
-
     if request.method == "POST":
-        print("POST DETECTED")
-
         name = request.form.get("name")
         email = request.form.get("email")
         message = request.form.get("message")
 
-        print("DATA:", name, email, message)
+        print(name, email, message)  # debug
+
+        return redirect(url_for("thank_you"))
 
     return render_template("index.html")
+
+
+@app.route("/thank-you")
+def thank_you():
+    return render_template("thank_you.html")
 
 
 if __name__ == "__main__":
