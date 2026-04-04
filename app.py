d0b44bf1b22e5941d8cfde45e3ba7e32d019c9c6
darledzinski-create <darledzinski@gmail.com>
2026-04-04 20:35:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    return render_template(first-consultation.html")
 
 @app.route("/consultation", methods=["GET", "POST"])
 def consultation():
