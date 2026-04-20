718e758519a609359cf2cad29a3795021fcde44c
darledzinski-create <darledzinski@gmail.com>
2026-04-20 16:47:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import request, redirect, url_for, render template
+from flask import request, redirect, url_for, render_template
 from datetime import datetime
 import os
 from mailjet_rest import Client
